03c33e1d887b04ed8e3b9244588befcf91964666
wyj3931 <wyj3931@gmail.com>
2023-11-09 13:26:44 +0100

    修复dhl bug

--- dhl.py
+++ dhl.py
@@ -98,47 +98,65 @@ def handle_dhl(fullpath, select_filename, tipps_value, root, progressbar):
         root.update()
 
         new_max_row = new_ws.max_row
+
         if ws.cell(i, col_list[3][1]).value == ws.cell(i - 1, col_list[3][1]).value:
             # 如果Identcode单号相同
-            for fee in col_list2:
-                #  附加费用遍历
-                if fee[0] == "Charge Amount":
-                    # print(ws.cell(i, fee[1]).value)
-                    if len(str(ws.cell(i, fee[1]).value)) == 9:
+            if ws.cell(i, col_list[3][1]).value is not None:
 
-                        if new_ws.cell(new_max_row, fee[2]).value is None:
-                            new_ws.cell(new_max_row, fee[2]).value = 0
-                        if ws.cell(i, col_cost).value is None:
-                            ws.cell(i, col_cost).value = 0
+                for fee in col_list2:
+                    #  附加费用遍历
+                    if fee[0] == "Charge Amount":
+                        # print(ws.cell(i, fee[1]).value)
+                        if len(str(ws.cell(i, fee[1]).value)) == 9:
 
-                        try:
-                            new_ws.cell(new_max_row, fee[2]).value = float(new_ws.cell(new_max_row, fee[2]).value) + float(ws.cell(i, col_cost).value)
-                        except ValueError:
-                            pass
-                            # print(new_max_row)
-                            # print(fee[2])
-                            # print(new_ws.cell(new_max_row, fee[2]).value)
-                            # print(ws.cell(i, col_cost).value)
-                if fee[0] == "Extra Charge Amount":
-                    if len(str(ws.cell(i, fee[1]).value)) < 9:
-                        if new_ws.cell(new_max_row, fee[2]).value is None:
-                            new_ws.cell(new_max_row, fee[2]).value = 0
-                        if ws.cell(i, col_cost).value is None:
-                            ws.cell(i, col_cost).value = 0
+                            if new_ws.cell(new_max_row, fee[2]).value is None:
+                                new_ws.cell(new_max_row, fee[2]).value = 0
+                            if ws.cell(i, col_cost).value is None:
+                                ws.cell(i, col_cost).value = 0
 
-                        try:
-                            new_ws.cell(new_max_row, fee[2]).value = float(new_ws.cell(new_max_row, fee[2]).value) + float(ws.cell(i, col_cost).value)
-                        except ValueError:
-                            pass
-                            # print(new_max_row)
-                            # print(fee[2])
-                            # print(new_ws.cell(new_max_row, fee[2]).value)
-                            # print(ws.cell(i, col_cost).value)
-            # print("total:"+str(col_list3[0][1]))
-            if ws.cell(i, col_list3[0][1]).value != 0:
-                # 主单号总金额
-                new_ws.cell(new_max_row, col_list3[0][2]).value = float(new_ws.cell(new_max_row, col_list3[0][2]).value) + float(ws.cell(i, col_list3[0][1]).value)
-                # print(new_ws.cell(new_max_row, col_list1[5][2]).value, " is ", float(ws.cell(i, col_list3[1][1]).value))
+                            try:
+                                new_ws.cell(new_max_row, fee[2]).value = float(new_ws.cell(new_max_row, fee[2]).value) + float(ws.cell(i, col_cost).value)
+                            except ValueError:
+                                pass
+                                # print(new_max_row)
+                                # print(fee[2])
+                                # print(new_ws.cell(new_max_row, fee[2]).value)
+                                # print(ws.cell(i, col_cost).value)
+                    if fee[0] == "Extra Charge Amount":
+                        if len(str(ws.cell(i, fee[1]).value)) < 9:
+                            if new_ws.cell(new_max_row, fee[2]).value is None:
+                                new_ws.cell(new_max_row, fee[2]).value = 0
+                            if ws.cell(i, col_cost).value is None:
+                                ws.cell(i, col_cost).value = 0
+
+                            try:
+                                new_ws.cell(new_max_row, fee[2]).value = float(new_ws.cell(new_max_row, fee[2]).value) + float(ws.cell(i, col_cost).value)
+                            except ValueError:
+                                pass
+                                # print(new_max_row)
+                                # print(fee[2])
+                                # print(new_ws.cell(new_max_row, fee[2]).value)
+                                # print(ws.cell(i, col_cost).value)
+                # print("total:"+str(col_list3[0][1]))
+
+                if ws.cell(i, col_list3[0][1]).value != 0:
+                    # 主单号总金额
+
+                    new_ws.cell(new_max_row, col_list3[0][2]).value = float(new_ws.cell(new_max_row, col_list3[0][2]).value) + float(ws.cell(i, col_list3[0][1]).value)
+                    # print(new_ws.cell(new_max_row, col_list1[5][2]).value, " is ", float(ws.cell(i, col_list3[1][1]).value))
+            else:
+                for x in range(len(col_list1)):
+                    new_ws.cell(new_max_row + 1, col_list1[x][2]).value = ws.cell(i, col_list1[x][1]).value
+                for fee in col_list2:
+                    if fee[0] == "Charge Amount":
+                        if len(str(ws.cell(i, fee[1]).value)) == 9:
+                            new_ws.cell(new_max_row + 1, fee[2]).value = ws.cell(i, col_cost).value
+                    if fee[0] == "Extra Charge Amount":
+                        if len(str(ws.cell(i, fee[1]).value)) < 9:
+                            new_ws.cell(new_max_row + 1, fee[2]).value = ws.cell(i, col_cost).value
+                for x in range(len(col_list3)):
+                    new_ws.cell(new_max_row + 1, col_list3[x][2]).value = ws.cell(i, col_list3[x][1]).value
+                # print(ws.cell(i, 53).value)
         else:
             for x in range(len(col_list1)):
                 new_ws.cell(new_max_row + 1, col_list1[x][2]).value = ws.cell(i, col_list1[x][1]).value
